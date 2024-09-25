a60e318ba0e9ed1a7b3b678d4bb22c92c082c0a4
aymantalukder <aymant@nycstudents.net>
2024-09-25 16:01:51 +0000

    Added Main,java

--- Feeder.java
+++ Feeder.java
@@ -12,6 +12,13 @@ public class Feeder {
      */
     public void simulateOneDay(int numBirds)
     {
+        if(Math.random() < 0.05) currentFood = 0;
+        else
+        {
+            int eaten = (int)(Math.random() * 41) + 10;
+            currentFood -= eaten * numBirds;
+            if(currentFood < 0) currentFood = 0;
+        }
     }
 
     /**
@@ -21,7 +28,15 @@ public class Feeder {
      */
     public int simulateManyDays(int numBirds, int numDays) 
     {
-        return 0;
+        int days = 0;
+        while(numDays > 0)
+        {
+            if(currentFood == 0) return days;
+            simulateOneDay(numBirds);
+            numDays--;
+            days++;
+        }
+        return days;
     }
     public int getCurrentFood()
     {
@@ -32,5 +47,10 @@ public class Feeder {
         currentFood = c;
     }
     public Feeder() {}
-  
+   
+    public String toString()
+    {
+        return "Current Food: " + currentFood;
+    }
+
 }
